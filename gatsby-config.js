73b10d229ab28554ea888f37eae16b33417c9dfa
ZTKpro <damianszmurlowork@gmail.com>
2021-09-17 11:00:50 +0200

    setup

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "busket-ace",
+  },
+  plugins: ["gatsby-plugin-sass"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import "../styles/index.scss";
+
+const IndexPage = () => {
+  return (
+    <main>
+      <section>
+        <h1>Sklep sobre</h1>
+      </section>
+    </main>
+  );
+};
+
+export default IndexPage;
